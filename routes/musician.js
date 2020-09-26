91fe1cbe35be3a6c09bd6fbb3c48d4528965b2d2
gopipravi <71910339+gopipravi@users.noreply.github.com>
2020-09-25 18:11:21 -0700

    Update musician.js
    Added the statement update to healthcheck

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("I am done with this, cool: ok!");
 });
 
 // retrieve all musicians from data store
